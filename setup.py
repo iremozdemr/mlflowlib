71fa4d0610d4b8e3ee8dcc915f7a45503ea15b21
İrem Özdemir <irem.ozdemir@etu.edu.tr>
2024-11-20 13:58:24 +0300

    old version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages, setup
 setup(
     name='mlflowlib',
     packages=find_packages(include=['mlflowlib']),
-    version='2.1',
+    version='2.2',
     license='MIT',    
     description='a general-purpose python package with MLflow integration',
     author='irem ozdemir',
@@ -14,5 +14,5 @@ setup(
         'tensorflow>=2.5.0',  
     ],
     url = 'https://github.com/iremozdemr/mlflowlib', 
-    download_url = 'https://github.com/iremozdemr/mlflowlib/archive/refs/tags/2.1.tar.gz',
+    download_url = 'https://github.com/iremozdemr/mlflowlib/archive/refs/tags/2.2.tar.gz',
 )
